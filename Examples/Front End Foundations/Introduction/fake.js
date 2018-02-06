91ca80b1a0740633376629c1899c68c24c82c325
Steve Bishop <xipooo@gmail.com>
2018-02-06 10:36:51 -0800

    Added examples to Introduction

---
+++ Examples/Front End Foundations/Introduction/fake.js
@@ -0,0 +1,17 @@
+const thisNumber = 1234;
+
+// thisNumber = 1200;
+
+// function addTwoNumbers(a, b){
+//     return a + b;
+// }
+
+const addTwoNumbers = function(a, b){
+    return a + b;
+}
+
+let sum = addTwoNumbers(5, 7);
+console.log(sum);
+
+sum = addTwoNumbers(1, 5);
+console.log(sum);
