fed5916ad3b1d50b0333669345547a9bb13d793c
Steve Bishop <xipooo@gmail.com>
2018-02-09 12:49:28 -0800

    Merge branch 'Introduction-To-JQuery'

--- Examples/Front End Foundations/Introduction To JQuery/jquery-starter/jquery.js
+++ Examples/Front End Foundations/Introduction To JQuery/jquery-starter/jquery.js
@@ -1,5 +1,19 @@
-$(document).ready(
-    function(){
-        $("p").hide();
-    }
-);
+// $(document).ready(function() { $("p").hide(); })
+$(function () {
+    //let pHidden = false;
+    // $("#hidePets").click(function () {
+    //     // if (pHidden) {
+    //     //     $("p").show();
+    //     //     pHidden = false;
+    //     // }
+    //     // else {
+    //     //     $("p").hide();
+    //     //     pHidden = true;
+    //     // }
+    //     $("p").toggle();
+    // });
+    $("p").click(function(){
+        //$(this).addClass("blue");
+        $(this).css("color", "orange");
+    });
+});
