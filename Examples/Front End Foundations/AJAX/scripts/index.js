67970d902f93fb393cd875c478bf2b24c3c84fc9
Steve Bishop <xipooo@gmail.com>
2018-02-12 11:14:38 -0800

    Added JQuery and JSON examples

--- Examples/Front End Foundations/AJAX/scripts/index.js
+++ Examples/Front End Foundations/AJAX/scripts/index.js
@@ -1,17 +1,23 @@
-function callback(xhttp){
-    document.getElementById("result").innerHTML = xhttp.responseText;
-}
+// function callback(xhttp){
+//     document.getElementById("result").innerHTML = xhttp.responseText;
+// }
 
-function loadDoc(){
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200){
-            callback(this);
-        }
-    }
-    xhttp.open("GET", "resources/_info.html", true);
-    xhttp.send();
-}
+// function loadDoc(){
+//     let xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200){
+//             callback(this);
+//         }
+//     }
+//     xhttp.open("GET", "resources/_info.html", true);
+//     xhttp.send();
+// }
 
-let changeButton = document.getElementById("changeButton");
-changeButton.onclick = loadDoc;
+// let changeButton = document.getElementById("changeButton");
+// changeButton.onclick = loadDoc;
+
+$(document).ready(function() {
+    $("#changeButton").click(function(){
+        $("#result").load("resources/_info.html");
+    });
+});
