a348550b2db09572a831e0e18fcf44e96ca768ec
Steve Bishop <xipooo@gmail.com>
2018-02-20 12:30:58 -0800

    Added Async example

---
+++ Examples/Front End Frameworks (React)/Async/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                <a href="/">Home</a>
+                <a href="/search">Search</a>
+                <a href="/mine">Mine</a>
+            </div>
+        );
+    }
+}
