0a5c054cf90ee53fea1e25b3f501d8a9165043b4
Steve Bishop <xipooo@gmail.com>
2018-02-26 10:47:05 -0800

    Update

---
+++ Examples/Programming Foundations (C#)/StaticTypeDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticTypeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(doStuff(5, 3));
+        }
+        static int doStuff(int x, int y){
+            return x * y;
+        }
+    }
+}
